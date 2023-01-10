b621d4c9b0431e48fe295dd411c9355bd93d6c15
Omkar Mhatre <mhatreomkar123@gmail.com>
2023-01-10 14:59:31 +0530

    Initial Commit

---
+++ GoogleOauthSecuritySpring/src/main/java/com/GoogleOauthSecuritySpring/GoogleOauthSecuritySpringApplication.java
@@ -0,0 +1,13 @@
+package com.GoogleOauthSecuritySpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleOauthSecuritySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleOauthSecuritySpringApplication.class, args);
+	}
+
+}
---
+++ GoogleOauthSecuritySpring/src/test/java/com/GoogleOauthSecuritySpring/GoogleOauthSecuritySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GoogleOauthSecuritySpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleOauthSecuritySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
